23dc1600198e3adf58c08244152f09d057b0d072
AdrianoR85 <adrianorosa085@gmail.com>
2024-02-21 21:05:18 -0300

    update while - created new functions

--- Lectures/while.c
+++ Lectures/while.c
@@ -1,52 +1,73 @@
 #include <stdio.h>
 
-float main() {
-    float score[4], avg, totalGrade;
-    float totalAvg = 0;
-    int option;
-    int nextStudent = 1;
+int menu() {
+  int option;
+  printf("\nAdd a new grades for a new student?");
+      printf("\n[0]: No");
+      printf("\n[1]: Yes");
+      printf("\nOption: ");
+      scanf("%d", &option);
+  return option;
+}
+
+float studentsGrades(int counter) {
+  float score, totalGrade;
+  totalGrade = 0.0;
+
+  printf("\nEnter the grade the student %d", counter);
+  printf("\n-------------------------------\n");
+    for (int i = 1; i <= 4; i++) {
+      printf("Enter the score %i: ", i);
+      scanf("%f", &score);
+      totalGrade += score;
+    }
+  
+  return totalGrade;
+}
+
+float result(float grades) {
+  float avg;
+  avg = grades / 4;
+
+  printf("\nAverage grade: %.2f\n", avg);
+  if (avg >= 7 ) {
+    printf("Passed\n");
+  } else {
+    printf("Failed\n");
+  }
+
+  return avg;
+}
+
+int main() {
+    int opt, grades;
+    float classAvg = 0;
+    int next = 1;
     int counter = 0;
 
     printf("=======================\n");
     printf("     Score Class\n");
     printf("=======================\n");
 
-    while(nextStudent == 1) {
-      printf("\nAdd a new grade for a new student?");
-      printf("\n[0]: No");
-      printf("\n[1]: Yes");
-      printf("\nOption: ");
-      scanf("%d", &option);
-
-      switch (option) {
+    while(next) {
+      opt = menu();
+      switch (opt) {
         case 0:
           printf("\nFinishing....\n\n");
-          nextStudent = 0;
+          next = 0;
           break;
         case 1:
           counter += 1;
-          totalGrade = 0;
-          printf("\nEnter the grade the student %d", counter);
-          printf("\n-------------------------------\n");
-          for (int i = 1; i <= 4; i++) {
-            printf("Enter the score %i: ", i);
-            scanf("%f", &score[i]);
-            totalGrade = totalGrade + score[i];
-          }
-          avg = totalGrade / 4;
-          printf("\nAverage grade: %.2f\n", avg);
-          if (avg >= 7 ) {
-            printf("Passed\n");
-          } else {
-            printf("Failed\n");
-          }
-          totalAvg += avg;
+          grades = studentsGrades(counter);
+          classAvg += result(grades);
           break;
-
         default:
           printf("\nInvalid option. Try again.\n");
           continue;
       }
     };
-  printf("Average grade of the Class: %.2f\n", totalAvg / counter);
+  
+  if (counter > 0) {
+    printf("Average grade of the Class: %.2f\n", classAvg / counter);
+  }
 } 
